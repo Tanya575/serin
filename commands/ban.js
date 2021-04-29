a47022a92c675e1eb8a1ca9b20c7caab9789afb3
Tanya575 <80748971+Tanya575@users.noreply.github.com>
2021-04-29 15:12:47 +0400

    ban

--- commands/ban.js
+++ commands/ban.js
@@ -1,16 +1,16 @@
 module.exports = {
     name: 'ban',
-    aliases: ['b', 'باند'],
-    permissions: ["ADMINISTRATOR", "BAN_MEMBERS"],
-    description: "this command bans a member!",
+    aliases: [],
+    permissions: [],
+    description: "ban command",
     execute(client, message, args){
         const member = message.mentions.users.first();
         if(member){
-            const memberTarget = message.guild.members.cache.get(member.id);
-            memberTarget.ban();
-            message.channel.send("<a:ench_pick:822221894195216385> that user has been banned");
+            const Target = message.guild.members.cache.get(member.id);
+            Target.ban();
+            message.channel.send('user has been banned');
         }else{
-            message.channel.send('<:idk:822217580567592991> I can t  find this member');
+            message.channel.send('make sure to mention a user after the command');
         }
     }
 }
