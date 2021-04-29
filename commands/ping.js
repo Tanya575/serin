0fa68b49d050fe3c98c2c27c0382f32f0e04ba93
Tanya575 <80748971+Tanya575@users.noreply.github.com>
2021-04-29 15:01:25 +0400

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'ping',
-    aliases: ['p', 'بنق'],
-    permissions: ["ADMINISTRATOR"],
-    description: "this is a ping command!",
+    aliases: [],
+    permissions: ['set permissions or put it null for everyone'],
+    description: "ping command",
     execute(client, message, args){
-        message.channel.send('pong!');
+        message.channel.send(`pong. \nLatency is ${Date.now() - message.createdTimestamp}ms \nAPI Letency is ${Math.round(client.ws.ping)}ms`);
     }
 }
