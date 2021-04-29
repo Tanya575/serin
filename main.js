2588073ca2024de9cec24e0e5906500b350a57c4
Tanya575 <80748971+Tanya575@users.noreply.github.com>
2021-04-29 15:25:16 +0400

    main

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-require('dotenv').config();
+require('dotenv').config(); // you need to install this package npm i dotenv
 const client = new Discord.Client({ partials: [ "MESSAGE", "CHANNEL", "REACTION" ]});
 
 
