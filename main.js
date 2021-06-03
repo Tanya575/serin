8772695645cd9084ca3a1dedd152a62002250c9d
Tanya <80748971+Tanya575@users.noreply.github.com>
2021-06-04 03:07:38 +0400

    ok~

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); 
 require('dotenv').config(); // install this package npm i dotenv (not required) 
 const client = new Discord.Client({ partials: [ "MESSAGE", "CHANNEL", "REACTION" ]});
 
@@ -10,6 +10,6 @@ client.events = new Discord.Collection();
 ['command_handler', 'event_handler'].forEach(h => {
     require(`./handlers/${h}`)(client, Discord);
 })
-
+ 
 // you can do this if you installed the package or you can just use ("bot token here")
 client.login(process.env.DISCORD_TOKEN);
